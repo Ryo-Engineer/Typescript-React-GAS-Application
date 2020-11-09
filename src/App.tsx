71867b41cccbfbdd8dd68b0a89043545c33142a1
Ryo-Engineer <r.yamanouchi.business@gmail.com>
2020-11-09 19:43:48 +0900

    commit -m "Last Commit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
+import Login from "./components/Login";
+import TopMenuControl from "./components/TopMenuControl";
+import SideMenuControl from "./components/SideMenuControl";
+import ObtainedAdditionalInformation from "./components/ObtainedAdditionalInformation";
+import Chat from "./components/Chat";
+import Remaining from "./components/Remaining";
+
 import './App.css';
+import './destyle.css';
+import debug from "./index";
+import {firestore} from "./firebase";
+import deps from "./data/department.json";
+
+export interface userStateProps {
+    name: string
+    password: string
+    team: string
+    department: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [userState, setUserState] = useState<userStateProps>({
+        name: '',
+        password: '',
+        team: 'Vermilion',
+        department: 'PlanningAndEvaluation'
+    });
+    const [auth, setAuth] = useState<boolean>(false);
+    const [remainingPeriod, setRemainingPeriod] = useState<number>(0);
+
+    const handleInputChange: React.ChangeEventHandler<HTMLInputElement | HTMLSelectElement> = (e) => {
+        e.persist();
+        const name = e.target.name;
+        const value = e.target.value;
+        setUserState((userState) => {
+            return {...userState, [name]: value};
+        });
+    }
+
+    const onButtonClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+        setSelectDep(userState.department);
+        setAuth(true);
+    }
+
+    const [selectDep, setSelectDep] = useState<string>('');
+
+    const onButtonClick2: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        deps.map((p) => {
+            if (p.logicalName === e.currentTarget.value) {
+                setSelectDep(p.logicalName);
+            }
+        });
+    };
+
+    useEffect(() => {
+        firestore.collection('remainingPeriod').doc(userState.team).collection(userState.department).onSnapshot((docs) => {
+            docs.forEach(doc => {
+                setRemainingPeriod(doc.data().remainingPeriod);
+            });
+        });
+    }, [userState]);
+
+    return (
+        <div className="App">
+            {!auth && <Login handleInputChange={handleInputChange} onButtonClick={onButtonClick}/>}
+            {auth &&
+            <div>
+                <TopMenuControl userTeam={userState.team} userDep={userState.department} userName={userState.name}
+                                selectDep={selectDep} onButtonClick2={onButtonClick2}/>
+                <SideMenuControl userTeam={userState.team} userDep={userState.department}
+                                 remainingPeriod={remainingPeriod} selectDep={selectDep}/>
+                <div className="container">
+                    <Chat {...userState}/>
+                    <ObtainedAdditionalInformation/>
+                    <Remaining userTeam={userState.team} userName={userState.name} remainingPeriod={remainingPeriod}/>
+                </div>
+            </div>
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import init from './init';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+
+export default function debug(input: any, select?: string) {
+    const debug = input;
+    switch (select) {
+        case 'a':
+            console.log(' debug_l: ' + debug);
+            console.dir('debug_d : ' + debug);
+            console.log(' debug_s: ' + JSON.stringify(debug));
+            break;
+        case 'l':
+            console.log(' debug_l: ' + debug);
+            break;
+        case 'd':
+            console.dir('debug_d : ' + debug);
+            break
+        case 's':
+            console.log(' debug_s: ' + JSON.stringify(debug));
+            break
+        case 'n':
+            break
+        default:
+            console.log(' debug_l: ' + debug);
+            console.dir('debug_d : ' + debug);
+            console.log(' debug_s: ' + JSON.stringify(debug));
+    }
+}
+
+
+init();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
